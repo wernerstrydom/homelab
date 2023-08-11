b4d389559baf8815ae8d6e4ef0502e6fb3b7ecc7
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-11 16:28:46 -0700

    Housekeeping

--- macos/apps/homebrew/setup.sh
+++ macos/apps/homebrew/setup.sh
@@ -8,10 +8,14 @@ else
     echo "--- Installing Homebrew"
     
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    export PATH=/opt/homebrew/bin:$PATH
+    export PATH=$(brew --prefix)/bin:$PATH
 fi
-# $(brew --prefix)
+
 grep -qxF "export PATH=$(brew --prefix)/bin:\$PATH" ~/.zshrc || echo "export PATH=$(brew --prefix)/bin:\$PATH" >> ~/.zshrc
+
+brew untap homebrew/core
+brew untap homebrew/cask
+
 brew upgrade
 
 PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
