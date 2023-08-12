2ff18ffca33529b03683e604c67119a1fab21818
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 07:37:58 -0700

    Adds Basg

---
+++ macos/apps/bash/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env ksh
+set -Eeuo pipefail
+
+SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+brew bundle --file=$SCRIPT_DIR/Brewfile
+
+echo $(brew --prefix)/bin/bash | sudo tee -a /etc/shells
+# chsh -s $(brew --prefix)/bin/bash
