2919dd82fa6e9f997b77ef6de790e437731136f0
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 07:46:11 -0700

    Refactor bootstrap.sh script for macOS
    - Added setup for bash and nano
    - Updated references to git, terraform, and vscode setup scripts
    
    The bootstrap.sh script for macOS has been refactored to include the setup for bash and nano. Additionally, the references to the git, terraform, and vscode setup scripts have been updated. This change improves the overall functionality of the script by including additional dependencies and ensuring that all relevant setup scripts are properly referenced.

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -4,14 +4,22 @@ sudo -v
 
 . ./apps/xcode-cli/setup.sh
 . ./apps/homebrew/setup.sh
+
 . ./apps/msoffice/setup.sh
 . ./apps/1password/setup.sh
+
 . ./apps/go/setup.sh
 . ./apps/python/setup.sh
-. ./apps/github/setup.sh
-. ./apps/ksh/setup.sh
 
+. ./apps/github/setup.sh
 . ./apps/git/setup.sh
+
+. ./apps/ksh/setup.sh
+. ./apps/bash/setup.sh
+
+. ./apps/terraform/setup.sh
+
+. ./apps/nano/setup.sh
 . ./apps/vscode/setup.sh
 . ./apps/jetbrains/setup.sh
-. ./apps/terraform/setup.sh
+
