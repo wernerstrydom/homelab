ddd63c30e871db37384252fb63780e50848c77ce
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 11:07:58 -0700

    Add Hazel and Elgato setup scripts to bootstrap.sh
    The commit adds the setup scripts for Hazel and Elgato to the bootstrap.sh file. This allows for the installation and configuration of these applications during the system bootstrap process.

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -16,7 +16,6 @@ fi
 
 . ./apps/msoffice/setup.sh
 . ./apps/1password/setup.sh
-. ./apps/elgato/setup.sh
 
 . ./system/ssh/setup.sh
 
@@ -40,3 +39,5 @@ fi
 . ./apps/vscode/setup.sh
 . ./apps/jetbrains/setup.sh
 
+. ./apps/elgato/setup.sh
+. ./apps/hazel/setup.sh
