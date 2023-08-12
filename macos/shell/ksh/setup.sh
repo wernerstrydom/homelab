119217f3225e335557db7361a74f49e34afd7cda
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 11:32:58 -0700

    Add zsh-autosuggestions and aliases to ksh setup
    The commit adds zsh-autosuggestions and aliases to the ksh setup. The Brewfile is updated to include "zsh-autosuggestions". In the setup.sh script, the variable "$HOMEBREW_PATH" is changed to "$HOMEBREW_PREFIX". The script also appends a line to the .zshrc file to source the zsh-autosuggestions plugin and adds aliases from a separate file.

--- macos/shell/ksh/setup.sh
+++ macos/shell/ksh/setup.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
 
-HOMEBREW_PATH=$(brew --prefix)
+$HOMEBREW_PREFIX=$(brew --prefix)
 if [ "$SHELL" != "$HOMEBREW_PATH/bin/ksh" ]; then
     grep -qxF "$HOMEBREW_PATH/bin/ksh" /etc/shells || echo "$HOMEBREW_PATH/bin/ksh" | sudo tee -a /etc/shells
     chsh -s $HOMEBREW_PATH/bin/ksh
@@ -18,13 +18,12 @@ else
     $HOMEBREW_PATH/bin/ksh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
-    pushd "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-    git pull origin
-    popd
-else
-    git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-fi
+grep -qxF "source \$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh" || echo "source \$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $HOME/.zshrc
 
 # Set OKSH Plugins
 sed -i '' '/^plugins=(.*/ s//plugins=\(git python 1password vscode github golang aws ansible zsh-autosuggestions\)/' "$HOME/.zshrc"
+
+echo "# ---------------------------------------------------------------------------------------------------------------" >> $HOME/.zshrc
+echo "# Aliases" >> $HOME/.zshrc
+echo "# ---------------------------------------------------------------------------------------------------------------" >> $HOME/.zshrc
+cat $SCRIPT_DIR/../../alias >> $HOME/.zshrc
