4bb0218449b85384ddebaf8680ab502431149880
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 15:52:49 -0700

    Change nano setup to use Homebrew prefix
    The commit changes the nano setup script to use the Homebrew prefix instead of the Homebrew path.

--- macos/apps/nano/setup.sh
+++ macos/apps/nano/setup.sh
@@ -4,18 +4,19 @@ set -Eeuo pipefail
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
 
-HOMEBREW_PATH=$(brew --prefix)
+HOMEBREW_PREFIX=$(brew --prefix)
 
 brew unlink nano && brew link nano
 
-alias nano=/opt/homebrew/bin/nano
-
-grep -qxF "alias nano=$HOMEBREW_PATH/bin/nano" ~/.zshrc || echo "alias nano=$HOMEBREW_PATH/bin/nano" >> ~/.zshrc
-grep -qxF "export EDITOR=$HOMEBREW_PATH/bin/nano" ~/.zshrc || echo "export EDITOR=$HOMEBREW_PATH/bin/nano" >> ~/.zshrc
-grep -qxF 'export VISUAL="$EDITOR"' ~/.zshrc || echo 'export VISUAL="$EDITOR"' >> ~/.zshrc
+grep -qxF 'alias nano=$HOMEBREW_PREFIX/bin/nano' ~/.zshrc || echo 'alias nano=$HOMEBREW_PREFIX/bin/nano' >> ~/.zshrc
+grep -qxF 'export EDITOR=$HOMEBREW_PREFIX/bin/nano' $HOME/.zshenv || echo 'export EDITOR=$HOMEBREW_PREFIX/bin/nano' >> $HOME/.zshenv
+grep -qxF 'export VISUAL="$EDITOR"' $HOME/.zshenv || echo 'export VISUAL="$EDITOR"' >> $HOME/.zshenv
 
 # Enable Syntax Highlighting
 touch ~/.nanorc
-grep -qxF "include \"$HOMEBREW_PATH/share/nano/*.nanorc\"" ~/.nanorc || echo "include \"$HOMEBREW_PATH/share/nano/*.nanorc\"" >> ~/.nanorc
+grep -qxF "include \"$HOMEBREW_PREFIX/share/nano/*.nanorc\"" ~/.nanorc || echo "include \"$HOMEBREW_PREFIX/share/nano/*.nanorc\"" >> $HOME/.nanorc
 
 git config --global core.editor "$HOMEBREW_PATH/bin/nano"
+
+source $HOME/.zshenv
+source $HOME/.zshrc
