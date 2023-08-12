e9ab3090e5b846e87382abf17c106521ad1a1bc0
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 11:55:21 -0700

    Refactor bootstrap.sh script for macOS
    The bootstrap.sh script for macOS has been refactored to improve readability and maintainability.
    
    - Added a "Prerequisites" section to install necessary dependencies
    - Organized the script into sections based on functionality, such as "Applications", "System Tools", "Languages", etc.
    - Added new setup scripts for GitKraken, Editors, and additional Applications
    - Improved overall code structure and formatting
    
    These changes make it easier to understand and modify the bootstrap.sh script in the future.

--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -13,15 +13,19 @@ else
   echo "Developer mode is already enabled"
 fi
 
+# Prerequisites
 . ./apps/xcode-cli/setup.sh
 . ./apps/homebrew/setup.sh
 . ./system/utils/setup.sh
 
+# Applications
 . ./apps/msoffice/setup.sh
 . ./apps/1password/setup.sh
 
+# System Tools
 . ./system/ssh/setup.sh
 
+# Languages
 . ./lang/c/setup.sh
 . ./lang/go/setup.sh
 . ./lang/python/setup.sh
@@ -30,17 +34,25 @@ fi
 . ./lang/kotlin/setup.sh
 . ./lang/ldb-mi/setup.sh
 
+# Source Control
 . ./apps/github/setup.sh
 . ./apps/git/setup.sh
 
+# Shells and Terminals
 . ./shell/ksh/setup.sh
 . ./shell/bash/setup.sh
 
 . ./apps/terraform/setup.sh
 
+# Development
+. ./development/gitkraken/setup.sh
+
+# Editors
+
 . ./apps/nano/setup.sh
 . ./apps/vscode/setup.sh
 . ./apps/jetbrains/setup.sh
 
+# Applications
 . ./apps/elgato/setup.sh
 . ./apps/hazel/setup.sh
