0f8e58808a6b7f4fb94964fbbee2bccab457c2b9
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 11:32:40 -0700

    Update Homebrew setup script
    The Homebrew setup script was updated to export the HOMEBREW_PREFIX variable and add it to the PATH in the .zshenv file. This ensures that Homebrew is properly configured and accessible. The changes also include sourcing the .zshenv file after modification.

--- macos/apps/homebrew/setup.sh
+++ macos/apps/homebrew/setup.sh
@@ -6,12 +6,13 @@ if command -v brew >/dev/null 2>&1; then
     brew update
 else
     echo "--- Installing Homebrew"
-    
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     export PATH=$(brew --prefix)/bin:$PATH
 fi
 
-grep -qxF "export PATH=$(brew --prefix)/bin:\$PATH" ~/.zshrc || echo "export PATH=$(brew --prefix)/bin:\$PATH" >> ~/.zshrc
+grep -qxF "export HOMEBREW_PREFIX=$(brew --prefix)" ~/.zshrc || echo "export HOMEBREW_PREFIX=$(brew --prefix)" >> $HOME/.zshenv
+grep -qxF "export PATH=\$HOMEBREW_PREFIX/bin:\$PATH" ~/.zshrc || echo "export PATH=\$HOMEBREW_PREFIX/bin:\$PATH" >> $HOME/.zshenv
+source $HOME/.zshenv
 brew upgrade
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
