73ba1d901c17b2d06cc0f891f2e6acafcba70698
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 10:54:02 -0700

    Update setup.sh to download and install the appropriate version of ldb-mi based on the system architecture.
    - Add support for ARM64 architecture
    - Update URL and binary path accordingly
    - Remove temporary files after installation

--- macos/lang/ldb-mi/setup.sh
+++ macos/lang/ldb-mi/setup.sh
@@ -4,12 +4,18 @@ set -Eeuo pipefail
 if [[ $(uname -m) == "arm64" ]]; then
   echo "Downloading ARM64 version of ldb-mi"
   URL="https://go.microsoft.com/fwlink/?linkid=2189794"
+  BINARY_PATH="/tmp/ldb-mi/debugAdapters/lldb-mi_arm64/bin/lldb-mi"
 else
   echo "Downloading Intel version of ldb-mi"
   URL="https://go.microsoft.com/fwlink/?linkid=2189795"
+  BINARY_PATH="/tmp/ldb-mi/debugAdapters/lldb-mi_x86_64/bin/lldb-mi"
+
 fi
 
 curl -L -o /tmp/ldb-mi.zip $URL
 unzip /tmp/ldb-mi.zip -d /tmp/ldb-mi
-sudo cp /tmp/ldb-mi/debugAdapters/lldb-mi/bin/lldb-mi /usr/local/bin/ldb-mi
+sudo cp $BINARY_PATH /usr/local/bin/ldb-mi
 chmod +x /usr/local/bin/ldb-mi
+rm -Rf /tmp/ldb-mi.zip /tmp/ldb-mi
+
+
