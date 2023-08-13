caa1f72269cfb1d85d008a2c9bed82319c322226
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 22:11:18 -0700

    Install or upgrade Homebrew
    This commit adds code to install or upgrade Homebrew. If Homebrew is not already installed, it will be installed using the installation script from the official Homebrew repository. If Homebrew is already installed, a message will be displayed indicating that it is already installed.
    
    The code also includes a check for the system architecture (arm64 or x86_64), but this part of the code is currently commented out and does not have any effect.
    
    No changes were made to the Brewfile or any other files in this commit.

--- macos/system/homebrew/setup.sh
+++ macos/system/homebrew/setup.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env zsh
 set -Eeuo pipefail
-# check arch and the paths
-if command -v brew >/dev/null 2>&1; then
-    echo "--- Upgrading Homebrew"
-    brew update
-else
+
+if ! command -v brew &> /dev/null
+then
     echo "--- Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "--- Homebrew already installed"
 fi
 
-echo 'eval $(brew shellenv)' > $HOME/.zprofile
-eval $(brew shellenv)
+brew shellenv
 
-brew upgrade
+# check if we're running on arm64 or x86_64
+# if [[ $(uname -m) == "arm64" ]]; then
+# else
+# fi
 
-SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-brew bundle --file=$SCRIPT_DIR/Brewfile
+
+
+# echo 'eval $(brew shellenv)' > $HOME/.zprofile
+# eval $(brew shellenv)
+
+# brew upgrade
+
+# SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# brew bundle --file=$SCRIPT_DIR/Brewfile
 
 
