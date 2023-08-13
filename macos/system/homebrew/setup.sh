51e3fb1bfddff61fa8924dc942f514326ec9a2f8
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-13 08:42:28 -0700

    Refactor Homebrew setup script for better compatibility
    The Homebrew setup script has been refactored to improve compatibility with different architectures. Instead of relying on the `uname -m` command, the script now checks whether we're running on arm64 or x86_64 by using the `$(uname -m)` variable directly. Additionally, the script now sets the `$HOMEBREW_PREFIX` variable based on the architecture and installs Homebrew accordingly.
    
    The paths file is updated to include the correct `$HOMEBREW_PREFIX/bin` and `$HOMEBREW_PREFIX/sbin` directories. The contents of `/etc/paths` are appended to ensure all necessary paths are included.
    
    Finally, the environment is updated by evaluating `"$($HOMEBREW_PREFIX/bin/brew shellenv)"`.

--- macos/system/homebrew/setup.sh
+++ macos/system/homebrew/setup.sh
@@ -1,55 +1,27 @@
 #!/usr/bin/env zsh
 set -Eeuo pipefail
 
-if ! command -v brew &> /dev/null
-then
-    echo "--- Installing Homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# check whether we're running on arm64 or x86_64
+if [[ $(uname -m) == "arm64" ]]; then
+  HOMEBREW_PREFIX=/opt/homebrew
 else
-    echo "--- Homebrew already installed"
+  HOMEBREW_PREFIX=/usr/local
+fi
+
+# check if $HOMEBREW_PREFIX/bin/brew exists
+if [ ! -f $HOMEBREW_PREFIX/bin/brew ]; then
+  echo "--- Installing Homebrew"
+  arch amd64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "--- Homebrew already installed"
 fi
 
 # Updating Paths
-echo "$(brew --prefix)/bin" > paths
-echo "$(brew --prefix)/sbin" >> paths
+echo "$HOMEBREW_PREFIX/bin" > paths
+echo "$HOMEBREW_PREFIX/sbin" >> paths
 cat /etc/paths >> paths
-cat paths | uniq > /etc/paths
+cat paths | uniq > uniq_paths
+cat uniq_paths
 
-# sudo mv /etc/paths /etc/paths.bak
-# sudo mv paths /etc/paths
-
-
-brew shellenv > $HOME/.zshrc
-source $HOME/.zshrc
-
-## check if .homebew.zsh exists
-#if [ ! -f $HOME/.homebrew.zsh ]; then
-#  echo "eval $(brew shellenv)" >> $HOME/.homebrew.zsh
-#fi
-#
-## check if .homebrew.zsh is sourced in .zshrc
-#if ! grep -qxF "source \$HOME/.homebrew.zsh" $HOME/.zshrc; then
-#  echo "source \$HOME/.homebrew.zsh" >> $HOME/.zshrc
-#fi
-#
-#eval $(brew shellenv)
-#
-#brew upgrade
-
-
-# check if we're running on arm64 or x86_64
-# if [[ $(uname -m) == "arm64" ]]; then
-# else
-# fi
-
-
-
-# echo 'eval $(brew shellenv)' > $HOME/.zprofile
-# eval $(brew shellenv)
-
-# brew upgrade
-
-# SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-# brew bundle --file=$SCRIPT_DIR/Brewfile
-
-
+# ensure this environment is up to date
+eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
