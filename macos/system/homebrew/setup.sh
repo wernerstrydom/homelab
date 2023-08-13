23bfa0efbea1ad023aaf7fe8d6d21280a638642c
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-12 21:06:55 -0700

    Refactor Homebrew setup script
    The Homebrew setup script was refactored to simplify the process and improve readability. The changes include:
    
    - Removed unnecessary checks for /etc/zshenv existence and creation
    - Removed redundant export statements for HOMEBREW_PREFIX and PATH variables in /etc/zshenv
    - Added a new line to set up the shell environment in .zprofile using 'eval $(brew shellenv)'
    
    These changes make the script more efficient and easier to maintain.

--- macos/system/homebrew/setup.sh
+++ macos/system/homebrew/setup.sh
@@ -7,20 +7,11 @@ if command -v brew >/dev/null 2>&1; then
 else
     echo "--- Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    export PATH=$(brew --prefix)/bin:$PATH
 fi
 
-# check if /etc/zshenv exists, if not create it
-if [ ! -f /etc/zshenv ]; then
-    echo "--- Creating /etc/zshenv"
-    sudo touch /etc/zshenv
-fi
+echo 'eval $(brew shellenv)' > /Users/homefolder/.zprofile
+eval $(brew shellenv)
 
-# check if /etc/zshenv has the HOMEBREW_PREFIX and PATH variables, if not add them
-grep -qxF "export HOMEBREW_PREFIX=$(brew --prefix)" /etc/zshenv || echo "export HOMEBREW_PREFIX=$(brew --prefix)" | sudo tee -a /etc/zshenv
-grep -qxF "export PATH=$(brew --prefix)/bin:\$PATH" /etc/zshenv || echo "export PATH=$(brew --prefix)/bin:\$PATH" | sudo tee -a /etc/zshenv
-
-source /etc/zshenv
 brew upgrade
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
