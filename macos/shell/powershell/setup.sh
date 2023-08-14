e4627bfdd53ae1649c9baaddc14956c145158660
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-14 14:26:56 -0700

    Add pwsh to /etc/shells if it is not already present
    Previously, the setup script for PowerShell on macOS added pwsh to /etc/shells unconditionally. This change modifies the script to first check if pwsh is already in /etc/shells before adding it. If pwsh is already present, a message indicating that it is already in /etc/shells will be displayed. If pwsh is not present, it will be added using `brew --prefix` and `sudo tee -a`. This change improves the setup process by avoiding duplicate entries in /etc/shells.

--- macos/shell/powershell/setup.sh
+++ macos/shell/powershell/setup.sh
@@ -4,4 +4,11 @@ set -Eeuo pipefail
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
 
-echo $(brew --prefix)/bin/pwsh | sudo tee -a /etc/shells
+# check if pwsh is already in /etc/shells
+if grep -Fxq "$(brew --prefix)/bin/pwsh" /etc/shells
+then
+    echo "pwsh is already in /etc/shells"
+else
+    echo "pwsh is not in /etc/shells"
+    echo $(brew --prefix)/bin/pwsh | sudo tee -a /etc/shells
+fi
