5f5fa8b1d46304b5d3540a8308746821d5aa9a89
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-14 14:26:34 -0700

    Add check for existing bash in /etc/shells
    This change ensures that duplicate entries are not added to `/etc/shells`, preventing potential issues with shell selection.

--- macos/shell/bash/setup.sh
+++ macos/shell/bash/setup.sh
@@ -4,5 +4,12 @@ set -Eeuo pipefail
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
 
-echo $(brew --prefix)/bin/bash | sudo tee -a /etc/shells
+# check if bash is already in /etc/shells
+if grep -Fxq "$(brew --prefix)/bin/bash" /etc/shells
+then
+    echo "bash is already in /etc/shells"
+else
+    echo "bash is not in /etc/shells"
+    echo $(brew --prefix)/bin/bash | sudo tee -a /etc/shells
+fi
 # chsh -s $(brew --prefix)/bin/bash
