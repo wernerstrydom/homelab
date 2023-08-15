f7a7abf55264ea19c43e0ce32c8b39176e28daf8
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-15 06:46:20 -0700

    Add 1password plugin to oh-my-zsh plugins
    The commit adds the necessary code to add the 1password plugin to the list of oh-my-zsh plugins in the ~/.zshrc file. It checks if the plugin is already present and updates the file accordingly. This change ensures that 1password functionality is available in the zsh shell environment.

--- macos/apps/1password/setup.sh
+++ macos/apps/1password/setup.sh
@@ -6,13 +6,12 @@ brew bundle --file=$SCRIPT_DIR/Brewfile
 
 HOMEBREW_PREFIX=$(brew --prefix)
 
-# extract the list of plugins from ~/.zshrc
-plugins=$(grep -oP '(?<=plugins=\().*(?=\))' ~/.zshrc)
-
-# check if 1password plugin is already in the list of plugins
-if [[ $plugins == *"1password"* ]]; then
-    echo "1password plugin is already in the list of plugins"
+# add 1password to oh-my-zsh plugins
+if grep -Fxq "1password" ~/.zshrc
+then
+    echo "1password is already in ~/.zshrc"
 else
-    echo "1password plugin is not in the list of plugins"
-    sed -i '' 's/plugins=(/plugins=(\n  1password /' ~/.zshrc
+    echo "1password is not in ~/.zshrc"
+    sed -i '' 's/plugins=(/plugins=(\n  1password/' ~/.zshrc
 fi
+
