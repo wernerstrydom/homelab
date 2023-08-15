6d909c9c03d8b28e6d3d9192874aec4adcdb843e
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-15 16:26:39 -0700

    Add vscode to ~/.zshrc if not already present
    This change ensures that "vscode" is included in the zsh configuration file as a plugin.

--- macos/development/vscode/setup.sh
+++ macos/development/vscode/setup.sh
@@ -3,3 +3,12 @@ set -Eeuo pipefail
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
+
+
+if grep -Fxq "  vscode" ~/.zshrc
+then
+    echo "vscode is already in ~/.zshrc"
+else
+    echo "vscode is not in ~/.zshrc"
+    sed -i '' 's/plugins=(/plugins=(\n  vscode/' ~/.zshrc
+fi
