03f81862b412491f4f06995143520f80d5ea84a8
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-14 20:58:39 -0700

    Refactor Homebrew setup script and update global paths
    The Homebrew setup script has been refactored to remove unnecessary code. The installation of Homebrew has also been simplified. Additionally, the logic for updating global paths has been removed as it is no longer needed. Finally, the command to set up the shell environment for Homebrew has been updated.
    
    - Refactored Homebrew setup script
    - Simplified installation process
    - Removed logic for updating global paths
    - Updated command to set up shell environment

--- macos/system/homebrew/setup.sh
+++ macos/system/homebrew/setup.sh
@@ -11,47 +11,10 @@ fi
 # check if $HOMEBREW_PREFIX/bin/brew exists
 if [ ! -f $HOMEBREW_PREFIX/bin/brew ]; then
   echo "--- Installing Homebrew"
-  arch amd64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "--- Homebrew already installed"
 fi
-
-# ---------------------------------------------------------------------------
-# Update Global Paths
-#
-# This allows anyone logged in to use homebrew
-# ---------------------------------------------------------------------------
-
-## create a temporary file to store paths
-#touch paths
-#
-## check if $HOMEBREW_PREFIX/bin exists in /etc/paths, and if not, add it to paths
-#if ! grep -qxF "$HOMEBREW_PREFIX/bin" /etc/paths; then
-#  echo "$HOMEBREW_PREFIX/bin" >> paths
-#fi
-#
-## check if $HOMEBREW_PREFIX/sbin exists in /etc/paths, and if not, add it to paths
-#if ! grep -qxF "$HOMEBREW_PREFIX/sbin" /etc/paths; then
-#  echo "$HOMEBREW_PREFIX/sbin" >> paths
-#fi
-#
-## set a trap to delete the temporary files regardless of an error or not
-#trap 'rm -f paths uniq_paths' EXIT
-#
-## append the contents of /etc/paths to paths
-#cat /etc/paths >> paths
-#
-## remove duplicate entries from paths
-#cat paths | uniq > uniq_paths
-#
-## check if /etc/paths is different from uniq_paths, and if so, update /etc/paths
-#if ! cmp -s /etc/paths uniq_paths; then
-#  echo "--- Updating /etc/paths"
-#  sudo mv /etc/paths /etc/paths.bak
-#  sudo mv uniq_paths /etc/paths
-#else
-#  echo "--- /etc/paths already up to date"
-#fi
 
 # ---------------------------------------------------------------------------
 # Setting Profiles  (zshrc, bashrc, etc)
@@ -79,4 +42,7 @@ if ! grep -qxF "eval \"\$($HOMEBREW_PREFIX/bin/brew shellenv)\"" ~/.bashrc; then
   echo "eval \"\$($HOMEBREW_PREFIX/bin/brew shellenv)\"" >> ~/.bashrc
 fi
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+
+
+
