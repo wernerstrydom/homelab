222dd182a0552b2aefbf7d84776ded9cb9fee670
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-15 16:24:48 -0700

    Add GitHub CLI to zshrc
    The script now checks if "gh" is already in ~/.zshrc and adds it if not.
    
    - Add check for "gh" in ~/.zshrc
    - Update ~/.zshrc with "gh" if not present

--- macos/development/github/setup.sh
+++ macos/development/github/setup.sh
@@ -3,3 +3,11 @@ set -Eeuo pipefail
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
+
+if grep -Fxq "  gh" ~/.zshrc
+then
+    echo "gh is already in ~/.zshrc"
+else
+    echo "gh is not in ~/.zshrc"
+    sed -i '' 's/plugins=(/plugins=(\n  gh/' ~/.zshrc
+fi
