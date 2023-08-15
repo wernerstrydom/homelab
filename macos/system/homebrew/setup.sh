0b918bdb44c7257b204e74ceaf604bdcf50563c8
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-14 19:59:33 -0700

    Refactor homebrew setup script
    The temporary file for storing paths is no longer created. The logic for checking and updating `/etc/paths` has been commented out. This change simplifies the script by removing unnecessary steps and improves readability.

--- macos/system/homebrew/setup.sh
+++ macos/system/homebrew/setup.sh
@@ -22,36 +22,36 @@ fi
 # This allows anyone logged in to use homebrew
 # ---------------------------------------------------------------------------
 
-# create a temporary file to store paths
-touch paths
-
-# check if $HOMEBREW_PREFIX/bin exists in /etc/paths, and if not, add it to paths
-if ! grep -qxF "$HOMEBREW_PREFIX/bin" /etc/paths; then
-  echo "$HOMEBREW_PREFIX/bin" >> paths
-fi
-
-# check if $HOMEBREW_PREFIX/sbin exists in /etc/paths, and if not, add it to paths
-if ! grep -qxF "$HOMEBREW_PREFIX/sbin" /etc/paths; then
-  echo "$HOMEBREW_PREFIX/sbin" >> paths
-fi
-
-# set a trap to delete the temporary files regardless of an error or not
-trap 'rm -f paths uniq_paths' EXIT
-
-# append the contents of /etc/paths to paths
-cat /etc/paths >> paths
-
-# remove duplicate entries from paths
-cat paths | uniq > uniq_paths
-
-# check if /etc/paths is different from uniq_paths, and if so, update /etc/paths
-if ! cmp -s /etc/paths uniq_paths; then
-  echo "--- Updating /etc/paths"
-  sudo mv /etc/paths /etc/paths.bak
-  sudo mv uniq_paths /etc/paths
-else
-  echo "--- /etc/paths already up to date"
-fi
+## create a temporary file to store paths
+#touch paths
+#
+## check if $HOMEBREW_PREFIX/bin exists in /etc/paths, and if not, add it to paths
+#if ! grep -qxF "$HOMEBREW_PREFIX/bin" /etc/paths; then
+#  echo "$HOMEBREW_PREFIX/bin" >> paths
+#fi
+#
+## check if $HOMEBREW_PREFIX/sbin exists in /etc/paths, and if not, add it to paths
+#if ! grep -qxF "$HOMEBREW_PREFIX/sbin" /etc/paths; then
+#  echo "$HOMEBREW_PREFIX/sbin" >> paths
+#fi
+#
+## set a trap to delete the temporary files regardless of an error or not
+#trap 'rm -f paths uniq_paths' EXIT
+#
+## append the contents of /etc/paths to paths
+#cat /etc/paths >> paths
+#
+## remove duplicate entries from paths
+#cat paths | uniq > uniq_paths
+#
+## check if /etc/paths is different from uniq_paths, and if so, update /etc/paths
+#if ! cmp -s /etc/paths uniq_paths; then
+#  echo "--- Updating /etc/paths"
+#  sudo mv /etc/paths /etc/paths.bak
+#  sudo mv uniq_paths /etc/paths
+#else
+#  echo "--- /etc/paths already up to date"
+#fi
 
 # ---------------------------------------------------------------------------
 # Setting Profiles  (zshrc, bashrc, etc)
