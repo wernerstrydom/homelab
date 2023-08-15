f0736e751748f1952182ff23bf3f9685a3ace84a
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-15 16:19:12 -0700

    Add docker-compose to oh-my-zsh plugins
    This commit adds docker-compose to the list of plugins in ~/.zshrc if it is not already present. This change ensures that docker-compose can be used with oh-my-zsh.

--- macos/development/docker/setup.sh
+++ macos/development/docker/setup.sh
@@ -4,6 +4,15 @@ set -Eeuo pipefail
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
 
+# add docker-compose to oh-my-zsh plugins
+if grep -Fxq "  docker-compose" ~/.zshrc
+then
+    echo "docker-compose is already in ~/.zshrc"
+else
+    echo "docker-compose is not in ~/.zshrc"
+    sed -i '' 's/plugins=(/plugins=(\n  docker-compose/' ~/.zshrc
+fi
+
 # add docker to oh-my-zsh plugins
 if grep -Fxq "  docker" ~/.zshrc
 then
@@ -12,3 +21,4 @@ else
     echo "docker is not in ~/.zshrc"
     sed -i '' 's/plugins=(/plugins=(\n  docker/' ~/.zshrc
 fi
+
