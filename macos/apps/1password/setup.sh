5f179e4b51a2e48175dff4dee7952655f6595dd1
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-15 05:53:40 -0700

    Add 1Password to plugins in ~/.zshrc
    The setup script now adds 1Password to the list of plugins in ~/.zshrc. This ensures that 1Password is loaded as a plugin when using zsh. The line `plugins=(` in ~/.zshrc is modified to include `1password` at the end of the list.

--- macos/apps/1password/setup.sh
+++ macos/apps/1password/setup.sh
@@ -3,3 +3,13 @@ set -Eeuo pipefail
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
+
+HOMEBREW_PREFIX=$(brew --prefix)
+
+# add 1password to plugins in ~/.zshrc. The plugins line should may look like this:
+# plugins=(
+#   git
+#   zsh-syntax-highlighting
+# )
+# and we want to add 1password to the end of the list
+sed -i '' 's/plugins=(/plugins=(\n  1password\n/' ~/.zshrc
