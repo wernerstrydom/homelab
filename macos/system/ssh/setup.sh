76e4a9e40a48cfb210a7cc2af9f7130da0b1b07c
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-15 17:02:04 -0700

    Refactor SSH setup script for macOS
    The SSH setup script for macOS has been refactored to improve security and ensure proper file permissions. The changes include creating the `~/.ssh` directory with restricted access, setting appropriate permissions for `authorized_keys` and `config` files, and ensuring that the script ends with a newline. These changes enhance the overall security of the SSH configuration on macOS systems.

--- macos/system/ssh/setup.sh
+++ macos/system/ssh/setup.sh
@@ -3,3 +3,10 @@ set -Eeuo pipefail
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+touch ~/.ssh/config
+chmod 600 ~/.ssh/config
