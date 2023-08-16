de485aa191daa90bb13b37e4334f04e58d632d84
Werner Strydom <bloudraak@users.noreply.github.com>
2023-08-15 21:15:43 -0700

    Change time settings and enable network time synchronization
    The commit changes the time.sh script to update the time settings on macOS. It enables automatic time and timezone updates by modifying specific preferences files. Additionally, it sets the clock format to "EEE d MMM HH:mm:ss" and enables network time synchronization using `sudo systemsetup -setusingnetworktime on`. The line `sudo systemsetup -setnetworktimeserver time.apple.com` is commented out, indicating that it was changed or removed.

--- macos/system/settings/time.sh
+++ macos/system/settings/time.sh
@@ -4,10 +4,9 @@ set -Eeuo pipefail
 sudo defaults write /Library/Preferences/com.apple.timezone.auto Active -bool YES
 sudo defaults write /private/var/db/timed/Library/Preferences/com.apple.timed.plist TMAutomaticTimeOnlyEnabled -bool YES
 sudo defaults write /private/var/db/timed/Library/Preferences/com.apple.timed.plist TMAutomaticTimeZoneEnabled -bool YES
-
-sudo systemsetup -setusingnetworktime on
-sudo systemsetup -setnetworktimeserver time.apple.com
-
 sudo defaults write com.apple.menuextra.clock IsAnalog -bool false
 sudo defaults write com.apple.menuextra.clock FlashDateSeparators -bool true
 sudo defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM HH:mm:ss"
+
+sudo systemsetup -setusingnetworktime on
+# sudo systemsetup -setnetworktimeserver time.apple.com
