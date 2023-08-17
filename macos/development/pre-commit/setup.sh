9ebb72f02ee0298339e7f947ba06aa69316c166d
wstrydom <werner.strydom@mac.com>
2023-08-16 21:05:21 -0700

    Add setup for pre-commit and git template directory
    This commit adds setup for the pre-commit hook and git template directory. It checks if the `~/.git-template/hooks` directory exists, and creates it if it doesn't. Similarly, it checks if the `~/.git-template` directory exists, and creates it if necessary. The global git configuration is then updated to use the `~/.git-template` as the template directory.

--- macos/development/pre-commit/setup.sh
+++ macos/development/pre-commit/setup.sh
@@ -3,3 +3,13 @@ set -Eeuo pipefail
 
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 brew bundle --file=$SCRIPT_DIR/Brewfile
+
+if [ -d ~/.git-template/hooks ]
+then
+    echo "~/.git-template/hooks already exists"
+else
+    echo "~/.git-template/hooks does not exist"
+    mkdir -p ~/.git-template/hooks
+fi
+
+pre-commit init-templatedir ~/.git-template
--- macos/prerequisites/git/setup.sh
+++ macos/prerequisites/git/setup.sh
@@ -20,3 +20,15 @@ else
     echo "git is not in ~/.zshrc"
     sed -i '' 's/plugins=(/plugins=(\n  git/' ~/.zshrc
 fi
+
+# configure git template directory. If it doesn't exist, create it
+if [ -d ~/.git-template ]
+then
+    echo "~/.git-template already exists"
+else
+    echo "~/.git-template does not exist"
+    mkdir -p ~/.git-template/hooks
+fi
+
+git config --global init.templateDir ~/.git-template
+
